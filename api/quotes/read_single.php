0b63fd8e2c85364e83330bd5de1c9ecf0b049964
Nicholas Pollard <nicholaslpollard@gmail.com>
2025-03-27 00:10:22 -0400

    Changes to CRUS/models.

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -1,6 +1,14 @@
 <?php
+// Include CORS and error handling headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    exit();
+}
 
 include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
